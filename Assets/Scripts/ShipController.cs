1ca13b8139c050f03303e6d49cfa88120849adc8
aspschou <57791851+aspschou@users.noreply.github.com>
2022-10-04 15:38:50 +0200

    Added new skybox and rocket animation

--- Assets/Scripts/ShipController.cs
+++ Assets/Scripts/ShipController.cs
@@ -4,8 +4,8 @@ using UnityEngine;
 
 public class ShipController : MonoBehaviour
 {
-    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f;
-    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed;
+    public float forwardSpeed = 25f, strafeSpeed = 7.5f, hoverSpeed = 5f, boostSpeed = 100f;
+    private float activeForwardSpeed, activeStrafeSpeed, activeHoverSpeed, activeBoostSpeed;
     public float forwardAcceleration = 2.5f, strafeAcceleration = 2f, hoverAcceleration = 2f;
     
     public float lookRotateSpeed = 90f;
@@ -13,7 +13,9 @@ public class ShipController : MonoBehaviour
 
     private float rollInput;
     public float rollSpeed = 90f, rollAcceleration = 3.5f;
-    
+
+    public ParticleSystem flame1, flame2, flame3;
+
     void Start()
     {
         Cursor.visible = false;
@@ -21,6 +23,7 @@ public class ShipController : MonoBehaviour
         
         screenCenter.x = Screen.width * .5f;
         screenCenter.y = Screen.height * .5f;
+        
     }
     
     void Update()
@@ -38,10 +41,16 @@ public class ShipController : MonoBehaviour
         transform.Rotate(-mouseDistance.y * lookRotateSpeed * Time.deltaTime, mouseDistance.x * lookRotateSpeed * Time.deltaTime, rollInput * rollSpeed * Time.deltaTime, Space.Self);
         
         activeForwardSpeed = Mathf.Lerp(activeForwardSpeed,Input.GetAxisRaw("Vertical") * forwardSpeed, forwardAcceleration * Time.deltaTime);
+        if (Input.GetAxisRaw("Boost") > 0)
+        {
+            activeBoostSpeed = Mathf.Lerp(1, Input.GetAxisRaw("Boost") * boostSpeed,
+                forwardAcceleration * Time.deltaTime);
+        }
+        else activeBoostSpeed = 1;
         activeStrafeSpeed = Mathf.Lerp(activeStrafeSpeed,Input.GetAxisRaw("Horizontal") * strafeSpeed, strafeAcceleration * Time.deltaTime);
         activeHoverSpeed = Mathf.Lerp(activeHoverSpeed,Input.GetAxisRaw("Hover") * hoverSpeed, hoverAcceleration * Time.deltaTime);
 
-        transform.position += transform.forward * activeForwardSpeed * Time.deltaTime;
+        transform.position += transform.forward * activeForwardSpeed * activeBoostSpeed * Time.deltaTime;
         transform.position += transform.right * activeStrafeSpeed * Time.deltaTime;
         transform.position += transform.up * activeHoverSpeed * Time.deltaTime;
     }
