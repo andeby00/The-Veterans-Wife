a1f65b475823311ce98a7e5a821477f146df1b7a
Rasmus <rasmuswium@hotmail.com>
2022-10-25 13:52:15 +0200

    idk added inventory?

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -1,11 +1,5 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Globalization;
-using System.Text;
 using TMPro;
 using Unity.VisualScripting;
-using UnityEditor;
-using UnityEditor.UIElements;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
@@ -13,7 +7,8 @@ public class PlayerMovement : MonoBehaviour
     Rigidbody _rb;
     [SerializeField] Transform groundCheck;
     [SerializeField] LayerMask ground;
-    [SerializeField] float movSpeed = 100f;
+    [SerializeField] float movSpeed = 10f;
+    [SerializeField] float maxSpeed = 100f;
     [SerializeField] float jumpHeight = 10f;
     [SerializeField] CharacterController controller;
     [SerializeField] float gravity = -9.81f;
@@ -32,18 +27,30 @@ public class PlayerMovement : MonoBehaviour
         var horizontal = Input.GetAxis("Horizontal");
         var vertical = Input.GetAxis("Vertical");
 
-        Vector3 move = transform.right * horizontal + transform.forward * vertical;
-        controller.Move(move * movSpeed * Time.deltaTime);
+        // Vector3 move = transform.right * horizontal + transform.forward * vertical;
+        // controller.Move(move * movSpeed * Time.deltaTime);
+        //
+        // _velocity.y += gravity * Time.deltaTime;
+        // controller.Move(_velocity * Time.deltaTime);
+
+        Vector3 vel = new Vector3(controller.velocity.x, 0, controller.velocity.z);
+        Vector3 wishDir = transform.right * horizontal + transform.forward * vertical;
+
+        float currentSpeed = Vector3.Dot(vel, wishDir);
+        
+        Vector3 move = wishDir * (movSpeed * Time.deltaTime);
+        // Vector3 move = vel + wishDir * ((movSpeed - currentSpeed) * Time.deltaTime);
 
         _velocity.y += gravity * Time.deltaTime;
-        controller.Move(_velocity * Time.deltaTime);
-        
+        move.y = _velocity.y * Time.deltaTime;
+        controller.Move(move);
 
+        
         // _rb.velocity = new Vector3(horizontal * movSpeed * Time.deltaTime, _rb.velocity.y, vertical * movSpeed * Time.deltaTime);
 
         if (IsGrounded() && _velocity.y < 0)
         {
-            _velocity.y = 0f; // -2f
+            _velocity.y = 0; // -2f
         }
         
         if (Input.GetButtonDown("Jump") && IsGrounded())    
@@ -51,11 +58,9 @@ public class PlayerMovement : MonoBehaviour
             Jump();
         }
 
-        velocityDiplay.SetText(transform.forward.ToShortString());//// skal testes, tror ik virker dog 
+        //velocityDiplay.SetText(controller.velocity.ToShortString());
         
-        
-        
-        // velocityDiplay.SetText(Mathf.Sqrt(Mathf.Pow(controller.velocity.x, 2) + Mathf.Pow(controller.velocity.z, 2)).ToShortString());
+        velocityDiplay.SetText(vel.ToShortString() + " - " + wishDir.ToShortString());
     }
 
     bool IsGrounded()
