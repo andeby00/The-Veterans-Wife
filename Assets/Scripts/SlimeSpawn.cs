839bf7da381cf6e4969cd4ea89aff5c2a4c0812c
m0rr47z <201905424@post.au.dk>
2023-01-19 19:34:57 +0100

    enemies ganging on this

---
+++ Assets/Scripts/SlimeSpawn.cs
@@ -0,0 +1,18 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlimeSpawn : EnemyAI
+{
+    public GameObject slimePrefab;
+    // Start is called before the first frame update
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+   
+}
